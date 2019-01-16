a48957ba39923a943ee35404162f9787850c2051
lironedri <lironush1717@gmail.com>
2019-01-16 11:05:08 +0200

    virtual

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@ using namespace server_side;
 int main(int argc, const char *argv[]) {
     Server* server = new MySerialServer();
     Searcher<SearchableMatrix, string>* algoType = new AlgoBestFirstSearch<Point>();
-    AlgoBestFirstSearch<Point>* = new AlgoBestFirstSearch<Point>();
     Solver<SearchableMatrix, string>* solver = new MatrixProblemSolver(algoType);
     CacheManager<string,string>* cacheManager = new FileCacheManager<string, string>("ps.txt");
     ClientHandler* clientHandler = new MatrixProblemClientHandler(solver, cacheManager);
